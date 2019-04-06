5c376e1573d171639bdc69c9e86e5c82659818eb
Patrick Wang-Freninger <github@freninger.at>
2019-04-06 23:37:57 +0200

    Fix running unit tests in docker

--- build.py
+++ build.py
@@ -4,6 +4,6 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=["x86_64"], build_types=["Debug"])
+    builder = ConanMultiPackager(archs=["x86_64"], build_types=["Debug"], docker_run_options='--cap-add SYS_PTRACE')
     builder.add_common_builds(pure_c=False)
     builder.run()
