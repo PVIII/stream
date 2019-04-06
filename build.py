16992a9a63ac797edf8ea30161fd5bb00c2ebbbd
Patrick Wang-Freninger <github@freninger.at>
2019-04-06 15:49:29 +0200

    Remove modification of Clang build
    Project does not build with Clang.

--- build.py
+++ build.py
@@ -6,6 +6,4 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"], build_types=["Debug"])
     builder.add_common_builds(pure_c=False)
-    builder.update_build_if(lambda build: build.settings["compiler"] == "clang" and build.settings["compiler.version"] <= "4.0",
-                            new_settings={"compiler.libcxx": "libc++"})
     builder.run()
