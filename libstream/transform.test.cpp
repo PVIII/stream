95678779f20198fdd64fb5fa12e4016327239068
Patrick Wang-Freninger <github@freninger.at>
2019-11-03 19:43:24 +0100

    Add cancel test for transform adaptor

--- libstream/transform.test.cpp
+++ libstream/transform.test.cpp
@@ -240,6 +240,76 @@ SCENARIO("Transformations with single values.")
     }
 }
 
+SCENARIO("Cancelling operations.")
+{
+    GIVEN("A write stream that adds one.")
+    {
+        write_mock writer;
+        auto s = stream::transform_write(writer, [](auto v) { return v + 1; });
+
+        WHEN("Single value asynchronous write is called.")
+        {
+            REQUIRE_CALL(writer, write(2)).LR_RETURN(writer.sender_);
+
+            write_callback_mock  callback_mock;
+            error_callback_mock  error_mock;
+            cancel_callback_mock cancel_mock;
+            auto                 sender = s.write(1);
+
+            write_token writer_token;
+            REQUIRE_CALL(writer.sender_, submit(ANY(write_token)))
+                .LR_SIDE_EFFECT(writer_token = _1);
+            sender.submit(write_token{error_mock, cancel_mock, callback_mock});
+
+            WHEN("The operation is cancelled.")
+            {
+                REQUIRE_CALL(writer.sender_, cancel());
+                sender.cancel();
+
+                WHEN("The cancel callback is invoked.")
+                {
+                    REQUIRE_CALL(cancel_mock, call());
+                    writer_token.cancelled();
+                }
+            }
+        }
+    }
+
+    GIVEN("A read stream that adds one.")
+    {
+        read_mock reader;
+        auto s = stream::transform_read(reader, [](auto v) { return v + 1; });
+
+        WHEN("Single value asynchronous read is called.")
+        {
+            REQUIRE_CALL(reader, read()).LR_RETURN(reader.sender_);
+
+            read_callback_mock   callback_mock;
+            error_callback_mock  error_mock;
+            cancel_callback_mock cancel_mock;
+            auto                 sender = s.read();
+
+            read_token<int> reader_token;
+            REQUIRE_CALL(reader.sender_, submit(ANY(read_token<int>)))
+                .LR_SIDE_EFFECT(reader_token = _1);
+            sender.submit(
+                read_token<int>{error_mock, cancel_mock, callback_mock});
+
+            WHEN("The operation is cancelled.")
+            {
+                REQUIRE_CALL(reader.sender_, cancel());
+                sender.cancel();
+
+                WHEN("The cancel callback is invoked.")
+                {
+                    REQUIRE_CALL(cancel_mock, call());
+                    reader_token.cancelled();
+                }
+            }
+        }
+    }
+}
+
 SCENARIO("Change the value type.")
 {
     GIVEN("A write stream that adds a pair.")
