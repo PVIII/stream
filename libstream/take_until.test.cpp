3def334a2ae8a8409e6f398d50bb606924b922a6
Patrick Wang-Freninger <github@freninger.at>
2019-12-05 22:53:12 +0100

    Add cancellation test for take_until adaptor

--- libstream/take_until.test.cpp
+++ libstream/take_until.test.cpp
@@ -60,3 +60,40 @@ SCENARIO("Reads.")
         }
     }
 }
+
+SCENARIO("Cancelling operations.")
+{
+    GIVEN("A stream that reads until 1.")
+    {
+        read_mock reader;
+        auto s = stream::take_until_read(reader, [](auto v) { return v == 1; });
+
+        WHEN("Range asynchronous read is called.")
+        {
+            REQUIRE_CALL(reader, read_(_)).SIDE_EFFECT(_1 = vector{3, 1, 2});
+
+            done_callback_mock   callback_mock;
+            error_callback_mock  error_mock;
+            cancel_callback_mock cancel_mock;
+            array                a{0, 0, 0};
+            auto                 sender = s.read(a);
+
+            base_token t;
+            REQUIRE_CALL(reader.range_sender_, submit(ANY(base_token)))
+                .LR_SIDE_EFFECT(t = _1);
+            sender.submit(base_token{error_mock, cancel_mock, callback_mock});
+
+            WHEN("The operation is cancelled.")
+            {
+                REQUIRE_CALL(reader.range_sender_, cancel());
+                sender.cancel();
+
+                WHEN("The cancel callback is invoked.")
+                {
+                    REQUIRE_CALL(cancel_mock, call());
+                    t.cancelled();
+                }
+            }
+        }
+    }
+}
